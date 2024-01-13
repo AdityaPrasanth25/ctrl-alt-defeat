599aa73768b75b090d180b3b51e23fe227c0c1c2
AdityaPrasanth25 <93645871+AdityaPrasanth25@users.noreply.github.com>
2024-01-14 02:44:15 +0530

    Add files via upload

---
+++ run_scripts.py
@@ -0,0 +1,5 @@
+import subprocess
+subprocess.run("text_generator.py",shell=True)
+subprocess.run("voiceover_gen.py",shell=True)
+subprocess.run("all_image_generator.py",shell=True)
+subprocess.run("video_generator.py",shell=True)
